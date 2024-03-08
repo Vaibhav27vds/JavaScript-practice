02d6f18b692068011f043946ec91c5471a2f590f
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-03-08 20:51:16 +0530

    json practice

---
+++ Basics-2/objects-3.js
@@ -0,0 +1,14 @@
+const course = {
+    courseName: "nextjs",
+    price: 999999,
+    courseInstructor: "Vaibhav"
+}
+
+console.log(course.courseInstructor)
+
+const {price: rs} = course
+
+console.log(rs);
+
+//  JSON: JavaScript Object Notation
+
